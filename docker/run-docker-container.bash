fdb4d6db673c8ab42cd79cd60924d334ceb81d46
YUN-CHIEN CHEN <ycpss91255@gmail.com>
2021-01-27 11:09:03 +0800

    init : environment init

---
+++ docker/run-docker-container.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# start sharing xhost
+xhost +local:root
+
+# run docker
+docker run --rm \
+  --net=host \
+  --ipc=host \
+  --gpus all \
+  --privileged \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  -v $HOME/.Xauthority:$docker/.Xauthority \
+  -v $HOME/workspace/digital_twin_pc_ws:$HOME/work \
+  -e XAUTHORITY=$home_folder/.Xauthority \
+  -e DISPLAY=$DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  -it --name "dt-pc-ros" $(id -un)/digital-twin-pc
+
+
