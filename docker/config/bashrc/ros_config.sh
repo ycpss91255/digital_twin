61b1aef02537186829860db0798f28106369bf3f
Yun-Chien Chen <ycpss91255@gmail.com>
2021-04-19 13:29:43 +0800

    adjust :　ros　ip

--- docker/config/bashrc/ros_config.sh
+++ docker/config/bashrc/ros_config.sh
@@ -2,6 +2,6 @@
 source /opt/ros/$ROS_DISTRO/setup.bash
 
 # localhost
-export ROS_MASTER_URI=http://localhost:11311
-export ROS_HOSTNAME=localhost
-export ROS_IP=localhost
+export ROS_MASTER_URI=http://163.13.164.186:11311
+export ROS_HOSTNAME=163.13.164.186
+export ROS_IP=163.13.164.186
