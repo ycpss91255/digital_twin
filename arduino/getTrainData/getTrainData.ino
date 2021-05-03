6a4f884b2125871ce4ec1edefb7a94f4cfb85623
YUN-CHIEN CHEN <ycpss91255@gmail.com>
2021-05-03 19:06:03 +0800

    adjust : arduino pub topic 489hz

--- arduino/getTrainData/getTrainData.ino
+++ arduino/getTrainData/getTrainData.ino
@@ -4,45 +4,44 @@
  ******************************/
 #include "ros_handle_config.h"
 /*******************************
- * Define control
- ******************************/
-// #define SER_PR
-/*******************************
  * Define
  ******************************/
-#define Ref_V 5.0
-/*******************************
- * Functions Declare
- ******************************/
-float V_Conversion(float);
-/*******************************
- * Variables Declare
- ******************************/
+// #define SER_PR
 
+#define Ref_V 5.0
+#define BAUD 57600
 /*******************************
  * Arduino main code
  ******************************/
 void setup() {
 #ifndef SER_PR
+  nh.getHardware()->setBaud(BAUD);
   nh.initNode();
   nh.advertise(MotorCmdV_pub);
 
 #else
-  Serial.begin(115200);
+  Serial.begin(BAUD);
 #endif
 }
 
 void loop() {
+  // Voltage divider rule V_out = V_in * (R2 / (R1 + R2)
+  // R1 = 30k Ohm
+  // R2 = 7.5k Ohm
+  // return (float)V / 1023.0 * (Ref_V * 5.0);
+
   // positive and negative
-  float pos_v = V_Conversion(analogRead(A0));
-  float neg_v = V_Conversion(analogRead(A1));
-  if (pos_v > neg_v) {
+  float pos_v = (float)analogRead(A0) / 40.92;
+  float neg_v = (float)analogRead(A1) / 40.92;
+
+  if (pos_v > neg_v)
     CmdV_msg.data = pos_v;
-  } else if (pos_v < neg_v) {
+  else if (pos_v < neg_v)
     CmdV_msg.data = -neg_v;
-  } else {
+  else
     CmdV_msg.data = 0;
-  }
+
+    // if ((micros() - time) >= 1500) {
 #ifndef SER_PR
   MotorCmdV_pub.publish(&CmdV_msg);
   nh.spinOnce();
@@ -51,14 +50,4 @@ void loop() {
   Serial.print("\t");
   Serial.println(CmdV_msg.data);
 #endif
-}
-
-/*******************************
- * Functions Definitions
- ******************************/
-float V_Conversion(int V) {
-  // Voltage divider rule V_out = V_in * (R2 / (R1 + R2)
-  // R1 = 30k Ohm
-  // R2 = 7.5k Ohm
-  return (float)V / 1023.0 * (Ref_V * 5.0);
 }
