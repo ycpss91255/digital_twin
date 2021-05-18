2b5a20478ce6abbc619ef067e1f07a458ae7a05b
YUN-CHIEN CHEN <ycpss91255@gmail.com>
2021-05-19 01:34:45 +0800

    del : extra comment

--- refenerce/tx_serial.cpp
+++ refenerce/tx_serial.cpp
@@ -44,26 +44,3 @@ int main(int argc, char **argv) {
   close(fd);
   return 0;
 }
-
-// #include <algorithm>
-// #include <iostream>
-// #include <vector>
-// #include <string>
-
-// int main() {
-//     std::vector<std::string> symbols = {"IF1209", "IF1210"};
-//     std::vector<const char *> chars(symbols.size());
-
-//     std::for_each(symbols.begin(), symbols.end(), [](std::string& s) {
-//         std::cout << s << std::endl;
-//     });
-
-//     std::transform(symbols.begin(), symbols.end(), chars.begin(),
-//     std::mem_fun_ref(&std::string::c_str));
-
-//     std::for_each(chars.begin(), chars.end(), [](const char *s) {
-//         std::cout << s << std::endl;
-//     });
-
-//     return 0;
-// }
