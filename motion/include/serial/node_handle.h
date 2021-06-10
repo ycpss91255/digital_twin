990102dd8c59fe872965a71f7aa1523821efd5cd
YUN-CHIEN CHEN <ycpss91255@gmail.com>
2021-06-10 23:10:34 +0800

    temp : local and remote sync

---
+++ motion/include/serial/node_handle.h
@@ -0,0 +1,50 @@
+#ifndef NodeHandle_H
+#define NodeHandle_H
+/*******************************
+ * Include system header files
+ ******************************/
+#include <string.h>
+#include <cstdio>
+#include <iostream>
+/*******************************
+ * Include ROS header files
+ ******************************/
+#include "ros/ros.h"
+/*******************************
+ ** Include msg header files
+ ******************************/
+#include "motion/IMU.h"
+#include "motion/MotorStates.h"
+/*******************************
+ * Define
+ ******************************/
+
+// #define DEBUG
+
+class MotionNodeHandle {
+ public:
+  MotionNodeHandle(int, char**, std::string);
+  ~MotionNodeHandle();
+
+ public:
+  // variable
+  geometry_msgs::Twist MotionCmd;
+  motion::FourMotorCmd MotorEnc;
+
+  // function
+  void pub_MotorEnc(motion::FourMotorCmd);
+  void pub_MotorSpeed(motion::FourMotorCmd);
+
+ private:
+  // variable
+  ros::NodeHandle* n;
+  ros::Publisher MotorEnc_pub;
+  ros::Publisher MotorSpeed_pub;
+  ros::Subscriber CmdVal_sub;
+
+  // function
+  void init();
+  void CmdVelBack(const geometry_msgs::Twist::ConstPtr&);
+};
+
+#endif  // NodeHandle_H
