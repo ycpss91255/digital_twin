0056ff172bafcf1686e1fa1530d9335e542c8303
YUN-CHIEN CHEN <ycpss91255@gmail.com>
2021-06-17 22:00:56 +0800

    add : test ok asynchronous_input.cpp

--- refenerce/asynchronous_input.cpp
+++ refenerce/asynchronous_input.cpp
@@ -1,13 +1,20 @@
-
-#include <fcntl.h>
-#include <stdio.h>
+#include <errno.h> /* Error number definitions */
+#include <fcntl.h> /* File control definitions */
+#include <stdio.h> /* Standard input/output definitions */
+#include <stdlib.h>
 #include <sys/signal.h>
 #include <sys/types.h>
-#include <termios.h>
-#include <unistd.h>
+#include <termios.h> /* POSIX terminal control definitions */
+#include <unistd.h>  /* UNIX standard function definitions */
 
-#define BAUDRATE B38400
-#define MODEMDEVICE "/dev/ttyS1"
+#include <cmath>
+#include <iostream>
+#include <stdexcept>  // std::out_of_range
+#include <string>
+#include <vector>
+
+#define BAUDRATE B115200
+#define MODEMDEVICE "/dev/ttyUSB0"
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
 #define TRUE 1
@@ -17,7 +24,7 @@ volatile int STOP = FALSE;
 void signal_handler_IO(int status); /* definition of signal handler */
 int wait_flag = TRUE;               /* TRUE while no signal received */
 
-main() {
+int main() {
   int fd, c, res;
   struct termios oldtio, newtio;
   struct sigaction saio; /* definition of signal action */
@@ -31,13 +38,11 @@ main() {
   }
 
   /* install the signal handler before making the device asynchronous */
-   saio.sa_handler = signal_handler_IO;
-   sigemptyset(&saio.sa_mask);   //saio.sa_mask = 0;
-   saio.sa_flags = 0;
-   saio.sa_restorer = NULL;
-   sigaction(SIGIO,&saio,NULL);
-
-
+  saio.sa_handler = signal_handler_IO;
+  sigemptyset(&saio.sa_mask);  // saio.sa_mask = 0;
+  saio.sa_flags = 0;
+  saio.sa_restorer = NULL;
+  sigaction(SIGIO, &saio, NULL);
 
   /* allow the process to receive SIGIO */
   fcntl(fd, F_SETOWN, getpid());
@@ -58,8 +63,9 @@ main() {
 
   /* loop while waiting for input. normally we would do something
      useful here */
+  uint64_t i = 0;
   while (STOP == FALSE) {
-    printf(".\n");
+    printf("%ld\n", i++);
     usleep(100000);
     /* after receiving SIGIO, wait_flag = FALSE, input is available
        and can be read */
@@ -67,7 +73,7 @@ main() {
       res = read(fd, buf, 255);
       buf[res] = 0;
       printf(":%s:%d\n", buf, res);
-      if (res == 1) STOP = TRUE; /* stop loop if only a CR was input */
+      // if (res == 1) STOP = TRUE; /* stop loop if only a CR was input */
       wait_flag = TRUE;          /* wait for new input */
     }
   }
@@ -82,5 +88,6 @@ main() {
 
 void signal_handler_IO(int status) {
   printf("received SIGIO signal.\n");
+  printf("status = %d\n", status);
   wait_flag = FALSE;
 }
