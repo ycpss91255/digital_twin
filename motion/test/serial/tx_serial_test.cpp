fed226fc57de89d1d62fe5db91eeba88af9f5083
YUN-CHIEN CHEN <ycpss91255@gmail.com>
2021-06-21 16:15:49 +0800

    adjust : change to pub argv

--- motion/test/serial/tx_serial_test.cpp
+++ motion/test/serial/tx_serial_test.cpp
@@ -6,16 +6,21 @@ int set_time = 25;  // (ms)
 
 int main(int argc, char **argv) {
   SerialInit("/dev/ttyUSB0", 115200);
-  vector<float> speed = {2,8,-2,9};
-
-  clock_t start = clock(), end;
-  while (1) {
-    end = clock();
-    // linux clock(us), windows clock(ms)
-    if ((double)(end - start) >= (set_time * 1000)) {
-      pub_MotorSpeed(speed);
-      start = end;
-    }
-
+  vector<float> speed(4, 0);
+  for (int i = 1; i < argc; i++) {
+    speed.at(i - 1) = strtol(argv[i], NULL, 10);
   }
+
+  pub_MotorSpeed(speed);
+  return 0;
+
+  // clock_t start = clock(), end;
+  // while (1) {
+  //   end = clock();
+  //   // linux clock(us), windows clock(ms)
+  //   if ((double)(end - start) >= (set_time * 1000)) {
+  //     pub_MotorSpeed(speed);
+  //     start = end;
+  //   }
+  // }
 }
