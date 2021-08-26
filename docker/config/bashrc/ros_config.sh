0d489802003ca0b389b6caa44e79ce38927d5515
cyc <ycpss91255@gmail.com>
2021-08-26 22:49:54 +0800

    adjust : ros config

--- docker/config/bashrc/ros_config.sh
+++ docker/config/bashrc/ros_config.sh
@@ -1,7 +1,5 @@
 # ros entrypoint
 source /opt/ros/$ROS_DISTRO/setup.bash
 
-# localhost
-export ROS_MASTER_URI=http://163.13.164.186:11311
-export ROS_HOSTNAME=163.13.164.186
-export ROS_IP=163.13.164.186
+export ROS_MASTER_URI=http://10.42.0.1:11311
+export ROS_HOSTNAME=10.42.0.1
--- docker/config/zsh/zshrc/ros_config.sh
+++ docker/config/zsh/zshrc/ros_config.sh
@@ -1,17 +1,5 @@
 # ros entrypoint
 source /opt/ros/$ROS_DISTRO/setup.zsh
 
-# iclab wired network
-# export ROS_MASTER_URI=http://163.13.164.182:11311
-# export ROS_HOSTNAME=163.13.164.182
-# export ROS_IP=163.13.164.182
-
-# iclab wired network
-# export ROS_MASTER_URI=http://192.168.68.105:11311
-# export ROS_HOSTNAME=192.168.68.105
-# export ROS_IP=192.168.68.105
-
-# localhost
-export ROS_MASTER_URI=http://localhost:11311
-export ROS_HOSTNAME=localhost
-export ROS_IP=localhost
+export ROS_MASTER_URI=http://10.42.0.1:11311
+export ROS_HOSTNAME=10.42.0.1
