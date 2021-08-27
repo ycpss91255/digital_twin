9e55edd75aedb5025c6e426a9e0f19236b448199
cyc <ycpss91255@gmail.com>
2021-08-28 05:20:52 +0800

    add : pub_topic node

---
+++ motion/test/pub_topic.cpp
@@ -0,0 +1,61 @@
+#include <sstream>
+
+#include "geometry_msgs/Twist.h"
+#include "motion/parameter.h"
+#include "ros/ros.h"
+
+void pub_fn(float, float, float, float);
+
+ros::Publisher chatter_pub;
+geometry_msgs::Twist msg;
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "talker");
+  ros::NodeHandle n;
+
+  chatter_pub = n.advertise<geometry_msgs::Twist>(MOTION_TOPIC_NAME, 1000);
+
+  sleep(1);
+
+  float speed = 10.0;
+  float time = 3;
+  printf("+y\n");
+  pub_fn(0, speed, 0, time);
+  pub_fn(0, 0, 0, 1);
+
+  printf("-x\n");
+  pub_fn(-speed, 0, 0, time);
+  pub_fn(0, 0, 0, 1);
+
+  printf("-y\n");
+  pub_fn(0, -speed, 0, time);
+  pub_fn(0, 0, 0, 1);
+
+  printf("+x\n");
+  pub_fn(speed, 0, 0, time);
+  pub_fn(0, 0, 0, 1);
+
+  //   ros::Rate loop_rate(10);
+  //   int count = 0;
+  //   while (ros::ok()) {
+  //     std_msgs::String msg;
+  //     std::stringstream ss;
+  //     ss << "hello world " << count;
+  //     msg.data = ss.str();
+  //     ROS_INFO("%s", msg.data.c_str());
+  //     chatter_pub.publish(msg);
+  //     ros::spinOnce();
+  //     loop_rate.sleep();
+  //     ++count;
+  //   }
+
+  return 0;
+}
+
+void pub_fn(float x, float y, float z, float t) {
+  msg.linear.x = x;
+  msg.linear.y = y;
+  msg.angular.z = z;
+  chatter_pub.publish(msg);
+  sleep(t);
+}
