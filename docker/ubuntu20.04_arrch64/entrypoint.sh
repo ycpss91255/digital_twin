bad3517e5ee8aab498485ba03cd543e8f1b516ec
cyc <ycpss91255@gmail.com>
2021-08-31 17:06:09 +0800

    add : entrypoint.sh atuo run

--- docker/ubuntu20.04_arrch64/entrypoint.sh
+++ docker/ubuntu20.04_arrch64/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-# rm -f /var/run/pigpio.pid
-# sudo pigpiod
+# source ~/.bashrc
+# source ~/work/devel/setup.bash
+
+# export ROS_MASTER_URI=http://10.42.0.1:11311
+# export ROS_HOSTNAME=10.42.0.2
+
+# echo $ROS_MASTER_URI
+
+# roslaunch motion serial.launch
 
 exec $@
-
-# exec /usr/local/bin/pigpiod -g -a 1 $@
