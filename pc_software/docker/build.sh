c1df1cd018c5529bf1d5b16e926787d28b15ef21
cyc <ycpss91255@gmail.com>
2023-03-15 11:31:18 +0800

    done: x86_64 docker container update

--- pc_software/docker/build.sh
+++ pc_software/docker/build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Get dependent parameters
+source $(dirname $(readlink -f $0))/get_param.sh
+
 export DOCKER_BUILDKIT=1
 
 # Build docker images
@@ -9,6 +12,7 @@ docker build -t ${user}/${IMAGE} \
     --build-arg GROUP=${group} \
     --build-arg GID=${gid} \
     --build-arg HARDWARE=${hardware} \
-    -f ${FILE_DIR}/Dockerfile ${FILE_DIR}/..
+    -f ${FILE_DIR}/"Dockerfile_${hardware}" ${FILE_DIR}
 
 #     --progress=plain \
+#     --no-cache \
--- pc_software/docker/get_param.sh
+++ pc_software/docker/get_param.sh
@@ -4,12 +4,28 @@
 FILE_DIR=$(dirname $(readlink -f $0))
 
 IMAGE=$(echo $(dirname $(readlink -f $0)) \
-    | awk -F/ '{for (i=NF; i>0; i--) {if ($i ~ /_ws$/) {print $i; exit}}}')
+    | awk -F/ '{
+        for (i=NF; i>0; i--) {
+            if ($i ~ /_ws$/) {
+                sub(/_ws$/,"",$i);
+                print $i;
+                exit
+            }
+        }
+    }')
 
 CONTAINER=${IMAGE}
 
 WS_PATH=$(echo $(dirname $(readlink -f $0)) \
-    | awk -v ws=${IMAGE}_ws -F/ '{for (i=1; i<=ws; i++) {printf "%s/", $i}}')
+    | awk -v ws=${IMAGE}_ws -F/ '{
+        for (i=1; i<=NF; i++) {
+            if ($i == ws){
+                break;
+            }
+            printf "%s/", $i
+        }
+        printf "%s/", ws
+    }')
 
 # Start sharing xhost
 # xhost +local:root
@@ -22,12 +38,21 @@ else
     GPU_FLAG=""
 fi
 
-echo "user=$(id -un)"
-echo "group=$(id -gn)"
-echo "uid=$(id -u)"
-echo "gid=$(id -g)"
-echo "hardware=$(uname -m)"
+# Get system parameter
+user=$(id -un)
+group=$(id -gn)
+uid=$(id -u)
+gid=$(id -g)
+hardware=$(uname -m)
+
+if [ "$1" = "debug" ];then
+echo "user=${user}"
+echo "group=${group}"
+echo "uid=${uid}"
+echo "gid=${gid}"
+echo "hardware=${hardware}"
 echo "WS_PATH=${WS_PATH}"
 echo "IMAGE=${IMAGE}"
 echo "CONTAINER=${CONTAINER}"
 echo "GPU_FLAG=${GPU_FLAG}"
+fi
