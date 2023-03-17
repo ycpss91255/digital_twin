6cb40aa36e45408ebe334747774049affd8a01fe
cyc <ycpss91255@gmail.com>
2023-03-17 09:43:20 +0800

    init: circuit branch

--- de0_nano/DE0_NANO_MotorDrive/burn/0.cpfile.sh
+++
@@ -1,2 +0,0 @@
-cp ../output_files/DE0_NANO.sof .
-cp ../qsys/software/motordriver_school/motordriver_school.elf .
--- pc_software/library/README.md
+++
@@ -1,5 +0,0 @@
-# Library
-
-## Quoted code
-
-- [rplidar_ros](https://github.com/robopeak/rplidar_ros)
