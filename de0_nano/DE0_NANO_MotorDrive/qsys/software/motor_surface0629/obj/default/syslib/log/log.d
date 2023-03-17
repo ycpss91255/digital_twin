46f439da6d007850f9c864946f14c3f172327583
cyc <ycpss91255@gmail.com>
2023-03-17 11:21:54 +0800

    init: software branch

--- de0_nano/DE0_NANO_MotorDrive/qsys/software/motor_surface0629/obj/default/syslib/log/log.d
+++
@@ -1,8 +0,0 @@
-obj/default/syslib/log/log.o: syslib/log/log.c syslib/log/log.h \
-  ../motor_surface0629_bsp/system.h ../motor_surface0629_bsp/linker.h
-
-syslib/log/log.h:
-
-../motor_surface0629_bsp/system.h:
-
-../motor_surface0629_bsp/linker.h:
