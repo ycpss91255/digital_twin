20a41f2dee1c4b9ed5665dcc53abc598ef7ef64a
cyc <ycpss91255@gmail.com>
2023-03-25 21:31:07 +0800

    adjust: set docker param script function, smarter than before

--- docker/docker_test/get_param.sh
+++ docker/docker_test/get_param.sh
@@ -1,40 +1,17 @@
 #!/usr/bin/env bash
 
-# Function to set the Docker image, container, and workspace path based on the directory of the current script
-#
-# Parameters:
-#   None
-#
-# Returns:
-#   None
-#
-# Outputs the following variables:
-#     FILE_DIR: the directory of the current script
-#     IMAGE: the name of the Docker image to use
-#     CONTAINER: the name of the Docker container to create
-#     WS_PATH: the path to the workspace directory to use
-#
+# Use workspace name as image and container name
+
 function set_docker_name() {
-    # Use workspace name as image and container name
     FILE_DIR=$(dirname $(readlink -f $0))
 
-    # Check if the `docker` folder has a suffix, such as `docker_xxx`
-    # If yes, extract the suffix and store it in the `IMAGE` variable
-    IMAGE=$(echo "${FILE_DIR}" | awk -F/ '{
-            for (i=NF; i>0; i--) {
-                if ($i ~ /^docker_/) {
-                    sub(/^docker_/,"",$i);
-                    print $i;
-                    exit
-                }
-            }
-        }')
+    # Confirm whether the docker folder has a suffix, such as "docker_xxx"
+    IMAGE=$(basename ${FILE_DIR} | cut -d '_' -f 2)
+#    IMAGE=$(basename ${FILE_DIR})
 
-    # Extract the path of the workspace folder and store it in the `WS_PATH` variable
     WS_PATH=$(echo "${FILE_DIR}" | rev | cut -d '/' -f 2- | rev)
 
-    # If the `docker` folder has no suffix, check if the workspace folder has a prefix, such as `xxx_ws`
-    # If yes, extract the prefix and store it in the `IMAGE` variable, and update the `WS_PATH` variable accordingly
+    # Docker folder has no suffix, so confirm whether workspace folder has a prefix, such as "xxx_ws"
     if [[ -z "${IMAGE}" ]]; then
         IMAGE=$(echo "${FILE_DIR}" | awk -F/ '{
                 for (i=NF; i>0; i--) {
@@ -46,27 +23,23 @@ function set_docker_name() {
                 }
             }')
 
-        WS_PATH=$(echo "${FILE_DIR}" | awk -v ws=${IMAGE}_ws -F/ 'BEGIN {found=0} {
-                for (i=1; i<=NF; i++) {
-                    if ($i == ws){
-                        found=1;
-                        break;
-                    }
-                    printf "%s/", $i
-                }
-                if (found) printf "%s/", ws
-            }')
+
+        # WS_PATH=$(echo "${FILE_DIR}" | awk -v ws=${IMAGE}_ws -F/ '{
+        #         for (i=1; i<=NF; i++) {
+        #             if ($i == ws){
+        #                 break;
+        #             }
+        #             printf "%s/", $i
+        #         }
+        #         printf "%s/", ws
+        #     }')
     fi
 
-    # If the workspace folder has no prefix/suffix, set the image name to `unknown`
-    # and the workspace path to the current directory
+    # Workspace folder has no prefix, so image use default name "unknown"
     IMAGE=${IMAGE:-unknown}
-    WS_PATH=${WS_PATH:-${FILE_DIR}}
-
-    # Set the container name to be the same as the image name
+    # docker container name equal to image name
     CONTAINER=${IMAGE}
 
-    # Print out the values of FILE_DIR, IMAGE, CONTAINER, and WS_PATH
     printf "%s %s %s %s" "${FILE_DIR}" "${IMAGE}" "${CONTAINER}" "${WS_PATH}"
 }
 
@@ -90,8 +63,6 @@ function check_nvidia() {
     else
         GPU_FLAG=""
     fi
-
-    # Print out the values of GPU_FLAG
     printf "%s" "${GPU_FLAG}"
 }
 
@@ -111,24 +82,16 @@ function check_nvidia() {
 #     hardware: the hardware architecture of the current system
 #
 function get_system_info() {
-    # Try to retrieve the current user from Docker using the `docker info` command and store it in the `user` variable
-    # If that fails, fall back to using the `id` command to get the current user
+    # Get system parameter
     user=$(docker info 2>/dev/null | grep Username | awk -F '[: ]' '{print $2}')
-    user=${${user}:-$(id -un)}
+    if [ -z "${user}" ]; then
+        user=$(id -un)
+    fi
 
-    # Retrieve the group of the current user using the `id` command and store it in the `group` variable
     group=$(id -gn)
-
-    # Retrieve the UID of the current user using the `id` command and store it in the `uid` variable
     uid=$(id -u)
-
-    # Retrieve the GID of the current user using the `id` command and store it in the `gid` variable
     gid=$(id -g)
-
-    # Retrieve the hardware architecture of the current system using the `uname` command and store it in the `hardware` variable
     hardware=$(uname -m)
-
-    # Print out the values of user, group, uid, gid and hardware
     printf "%s %s %d %d %s" "${user}" "${group}" "${uid}" "${gid}" "${hardware}"
 }
 
@@ -166,14 +129,14 @@ done
 
 # $DEBUG && set -x
 
-# TODO: wait check
-# Start sharing xhost
-# xhost +local:root
-
+# GPU_FLAG=$(check_nvidia)
 read -r GPU_FLAG <<<"$(check_nvidia)"
 read -r user group uid gid hardware <<<"$(get_system_info)"
 read -r FILE_DIR IMAGE CONTAINER WS_PATH <<<"$(set_docker_name)"
 
+# TODO: wait check
+# Start sharing xhost
+# xhost +local:root
 
 if [ "$DEBUG" = true ]; then
     echo -e "GPU_FLAG=${GPU_FLAG}\n"
@@ -187,5 +150,5 @@ if [ "$DEBUG" = true ]; then
     echo "FILE_DIR=${FILE_DIR}"
     echo "WS_PATH=${WS_PATH}"
     echo "IMAGE=${IMAGE}"
-    echo -e "CONTAINER=${CONTAINER}\n"
+    echo -e "CONTAINER=${CONTAINER}"
 fi
