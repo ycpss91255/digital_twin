bf7503d94aa57f4a3f6138829d16206c8f939b0a
cyc <ycpss91255@gmail.com>
2023-03-29 06:25:21 +0800

    adjust: ros IP change to localhost

--- docker/config/script/ros_setup.sh
+++ docker/config/script/ros_setup.sh
@@ -6,11 +6,11 @@
 
 # add ROS env setting to bash config
 ROS_TYPE=$(echo ${1} | tr [a-z] [A-Z])
-echo "source /opt/ros/"${ROS_DISTRO}"/setup.bash" >>/home/"${USER}"/.bashrc
-echo "export ROS_MASTER_URI=http://${2}:11311" >>/home/"${USER}"/.bashrc
+echo "source /opt/ros/"${ROS_DISTRO}"/setup.bash" >> /home/"${USER}"/.bashrc
+echo "export ROS_MASTER_URI=http://${2}:11311" >> /home/"${USER}"/.bashrc
 
 if [ ${ROS_TYPE} == "MASTER" ]; then
-    echo "export ROS_HOSTNAME=${2}" >>/home/${USER}/.bashrc
+    echo "export ROS_HOSTNAME=${2}" >> /home/${USER}/.bashrc
 elif [ ${ROS_TYPE} == "SLAVE" ]; then
-    echo "export ROS_HOSTNAME=${3}" >>/home/${USER}/.bashrc
+    echo "export ROS_HOSTNAME=${3}" >> /home/${USER}/.bashrc
 fi
