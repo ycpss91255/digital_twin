7aafee609151aa82afdf9842a253f8c20d958014
cyc <ycpss91255@gmail.com>
2023-05-18 10:37:54 +0800

    init: ros2 env and test node

--- docker_ros2_coppeliasim/config/script/ros2_setup.sh
+++ docker_ros2_coppeliasim/config/script/ros2_setup.sh
@@ -4,17 +4,16 @@
 # ${2}: ROS2 network localhost or global
 # ${3}: ROS2 global numble
 
-echo -e "\nsource /opt/ros/"${1}"/setup.bash" >> /home/"${USER}"/.bashrc
-
+echo -e "\nsource /opt/ros/"${1}"/setup.bash\n\
+source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash\n\
+alias wb='source ~/work/install/local_setup.bash && echo \"You source workspace config!\"'\n" >> /home/"${USER}"/.bashrc
 # add ROS2 env setting to bash config
 ROS_TYPE=$(echo ${2} | tr [a-z] [A-Z])
 
 if [[ ${ROS_TYPE} == "LOCALHOST" ]]; then
     echo "export ROS_LOCALHOST_ONLY=1" >> /home/"${USER}"/.bashrc
+elif [[ -z ${2} ]]; then
+    echo "export ROS_DOMAIN_ID=0" >> /home/"${USER}"/.bashrc
 else
-    if [[ -z ${2} ]]; then
-        echo "export ROS_DOMAIN_ID=0" >> /home/"${USER}"/.bashrc
-    else
-        echo "export ROS_DOMAIN_ID=${3}" >> /home/"${USER}"/.bashrc
-    fi
+    echo "export ROS_DOMAIN_ID=${3}" >> /home/"${USER}"/.bashrc
 fi
