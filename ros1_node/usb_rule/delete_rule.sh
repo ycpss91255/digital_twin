a769e55cb9958f4998b8588e4cb8b1fed1d99780
cyc <ycpss91255@gmail.com>
2024-03-08 06:47:02 +0800

    adjust: ros1 related things ard moved to ros1_node

---
+++ ros1_node/usb_rule/delete_rule.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+file_dir=$(dirname $0)
+let len=${#file_dir}+1
+rule_list=$(ls $file_dir/*.rules)
+
+# Delete the created rule
+for i in $rule_list; do
+    rule=${i:len}
+    sudo rm /etc/udev/rules.d/$rule
+done
+
+# Reload usb rule
+sudo udevadm control --reload-rules
+udevadm trigger
+
+echo "delete usb rule complete"
